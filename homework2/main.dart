bd00b37ddddc0d3c0415e94333afc184cbfdc429
يزيد دوش <opopom123@gmail.com>
2026-02-10 22:50:11 +0300

    Create Flutter layout for campground demo
    This Flutter application features a layout with an image section, title section, button section, and text section, showcasing a campground.

---
+++ homework2/main.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const String appTitle = 'Flutter layout demo';
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(appTitle)),
+        body: const SingleChildScrollView(
+          child: Column(
+            children: [
+          
+              ImageSection(image: '\lake.jpg '),
+              TitleSection(
+                name: 'Oeschinen Lake Campground',
+                location: 'Kandersteg, Switzerland',
+              ),
+              ButtonSection(),
+            
+              TextSection(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ImageSection extends StatelessWidget {
+  const ImageSection({super.key, required this.image});
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      image,
+      width: double.infinity,
+      height: 240,
+      fit: BoxFit.cover,
+    );
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({super.key, required this.name, required this.location});
+
+  final String name;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32).copyWith(bottom: 16), 
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    name,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(location, style: TextStyle(color: Colors.grey[500])),
+              ],
+            ),
+          ),
+          Icon(Icons.star, color: Colors.red[500]),
+          const Text('41'),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Theme.of(context).primaryColor;
+    return Padding(
+      padding:
+          const EdgeInsets.symmetric(horizontal: 32), 
+      child: SizedBox(
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ButtonWithText(color: color, icon: Icons.call, label: 'CALL'),
+            ButtonWithText(color: color, icon: Icons.near_me, label: 'ROUTE'),
+            ButtonWithText(color: color, icon: Icons.share, label: 'SHARE'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ButtonWithText extends StatelessWidget {
+  const ButtonWithText({
+    super.key,
+    required this.color,
+    required this.icon,
+    required this.label,
+  });
+
+  final Color color;
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+
+class TextSection extends StatelessWidget {
+  const TextSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Oeschinen is a campsite nestled deep in the '
+        'Alpine lakes. Situated 1,578 meters above '
+        'sea level, Oeschinen offers a gondola ride '
+        'from Kandersteg, and pine forests up to 20° '
+        'Celsius in the summer. Activities enjoyed here '
+        'include hiking tobogga'
+       ' n runs, and summer'
+        
+        'toboggan.',
+        softWrap: true,
+        style: TextStyle(color: Colors.grey[700]),
+      ),
+    );
+  }
+}
